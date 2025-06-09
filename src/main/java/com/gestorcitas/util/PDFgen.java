81b1ffdb6667d2f8315a4bd919b1dbb865c0850b
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-06-08 20:33:12 -0500

    FInished the rough sketch for the PDF generation method

--- src/main/java/com/gestorcitas/util/PDFgen.java
+++ src/main/java/com/gestorcitas/util/PDFgen.java
@@ -2,8 +2,10 @@ package com.gestorcitas.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import com.gestorcitas.modelo.Cita;
+import com.gestorcitas.modelo.Doctor;
 import com.gestorcitas.modelo.Paciente;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -29,7 +31,10 @@ public class PDFgen {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfWriter.getInstance(document, baos);
 
-        // Paciente paciente = 
+        Paciente paciente = cita.getPaciente();
+        Doctor doctor = cita.getDoctor();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         document.open();
 
@@ -40,9 +45,23 @@ public class PDFgen {
 
         document.add(titulo);
 
+        //Info del paciente
         document.add(new Paragraph("INFORMACION DEL PACIENTE", SUBTITULO));
-        // document.add(new Paragraph("Nombre: " + , TEXTO_NORMAL));
+        document.add(new Paragraph("Nombre: " + paciente.getNombres() + paciente.getApellidos(), TEXTO_NORMAL));
+        document.add(new Paragraph("Documento de identidad: " + paciente.getDni(), TEXTO_NORMAL));
 
+        //Info de la cita
+        document.add(new Paragraph("INFORMACION DE LA CITA", SUBTITULO));
+        document.add(new Paragraph("Medico: " + doctor.getNombres() + doctor.getApellidos(), TEXTO_NORMAL));
+        document.add(new Paragraph("Fecha: " + cita.getFecha(), TEXTO_NORMAL));
+        document.add(new Paragraph("Hora: " + cita.getHora(), TEXTO_NORMAL));
+
+        document.add(new Paragraph("Fecha de generación: " + sdf.format(new java.util.Date()), TEXTO_PEQUEÑO));
+
+
+        document.close();
+
+        return baos.toByteArray();
 
 
     }
